ec687b11bbdc26dc14abc2fcd78b5285be2534b5
18373457821tgh <2380499075@qq.com>
2020-12-24 15:11:57 +0800

    这是我的第一次提交

---
+++ src/com/dao/yidu/demo.java
@@ -0,0 +1,13 @@
+package com.dao.yidu;
+
+/**
+ * @author 胖迪
+ * @version 1.0
+ * @description:
+ * @2020/12/24 15:09
+ */
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("靓女");
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,11 @@
+/**
+ * @author 胖迪
+ * @version 1.0
+ * @description:
+ * @2020/12/24 15:02
+ */
+public class main {
+    public static void main(String[] args) {
+        System.out.println("靓仔");
+    }
+}
